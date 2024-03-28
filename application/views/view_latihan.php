44f160da52c40bbe1e9860558caece3330386874
HaiikallR <haikalsitafajriramadhan@gmail.com>
2024-03-28 23:17:56 +0700

    first commit

---
+++ application/views/view_latihan.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title>Latihan 1</title>
+</head>
+<body>
+    Halo Kawan.. Yuk kita belajar web programming..!!!<br>
+    Nilai 1 = <?= $nilai1; ?><br> <!-- Menambahkan tag <br> untuk pemisah baris -->
+    Nilai 2 = <?= $nilai2; ?><br> <!-- Menambahkan tag <br> untuk pemisah baris -->
+    Ini hasil dari pemodelan dengan metode penjumlahan yaitu <?= $nilai1 ?> + <?= $nilai2 ?> = <?= $hasil; ?> <!-- Mengubah penggunaan string concatenation menjadi multiple echo -->
+</body>
+</html>
